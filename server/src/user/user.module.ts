537dbc61a570b43ece998e2c3184bc9b1cc51b1e
Mathéo <dasrah.dev@gmail.com>
2025-07-20 21:44:16 +0200

    global jwt guard in user routes, public decorator

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { PrismaService } from '../prisma.service';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from '../auth/jwt.auth-guard';
 
 @Module({
-  providers: [UserService, PrismaService],
+  providers: [
+    UserService,
+    PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
   exports: [UserService],
 })
 export class UserModule {}
