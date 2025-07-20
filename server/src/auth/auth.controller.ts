f88474f0cd76dd59b0eaf51e49559f1983b110c3
Mathéo <dasrah.dev@gmail.com>
2025-07-21 00:13:25 +0200

    add public access decorator to login and register routes

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -2,17 +2,20 @@ import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './local.auth-guard';
 import { AuthService } from './auth.service';
 import { AuthRequest } from '../types';
+import { Public } from '../public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('login')
   login(@Req() req: AuthRequest) {
     return this.authService.login(req.user);
   }
 
+  @Public()
   @Post('register')
   async register(@Body() req: { username: string; password: string }) {
     const { username, password } = req;
